fb3ecb2c448166a7bef3e8b135d3fb9fd05d2472
swapnil2993 <swapnilpande96@gmail.com>
2024-08-06 19:50:23 +0530

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,9 @@
-import { TriangleAlert } from "lucide-react";
 import "./app.scss";
-import Header from "./components/header";
-import Pagination from "./components/pagination";
-import Search from "./components/search";
-import Table from "./components/table";
-import useUsersData from "./hooks";
-import loaderUrl from "./loader.svg";
 
 function App() {
-  const {
-    users,
-    isLoading,
-    error,
-    searchQuery,
-    setSearchQuery,
-    paginate,
-    selectedUserIds,
-    totalPages,
-    currentPage,
-    deleteUserByIds,
-    handleSetSelectedIds,
-    handleBulkDelete,
-    updateUser,
-  } = useUsersData();
-
   return (
     <div className="container-wrapper">
-      <Header />
-      <main className="container">
-        {error ? (
-          <div className="info-container">
-            <TriangleAlert size={20} strokeWidth={1} absoluteStrokeWidth />
-            {error?.message || "Something went wrong"}
-          </div>
-        ) : isLoading ? (
-          <div className="info-container">
-            <img src={loaderUrl} width={"100px"} alt={"Loader"} />
-          </div>
-        ) : (
-          <>
-            <Search query={searchQuery} handleSearch={setSearchQuery} />
-            <Table
-              data={users}
-              handleDelete={deleteUserByIds}
-              handleRowSelect={handleSetSelectedIds}
-              selectedUsers={selectedUserIds}
-              updateUser={updateUser}
-            />
-            {selectedUserIds.length ? (
-              <div className="delete-container">
-                <button
-                  data-testid="bulk-delete"
-                  className="delete-btn"
-                  onClick={handleBulkDelete}
-                >
-                  Delete Selected
-                </button>
-              </div>
-            ) : null}
-            {totalPages > 0 && (
-              <Pagination
-                totalPages={totalPages}
-                paginate={paginate}
-                currentPage={currentPage}
-              />
-            )}
-          </>
-        )}
-      </main>
+      <main className="container">Hello</main>
     </div>
   );
 }
--- src/components/checkbox/index.tsx
+++
@@ -1,9 +0,0 @@
-import "./checkbox.scss";
-
-interface Props extends React.HTMLProps<HTMLInputElement> {}
-
-const Checkbox = (inputProps: Props) => {
-  return <input type="checkbox" className="checkbox" {...inputProps} />;
-};
-
-export default Checkbox;
